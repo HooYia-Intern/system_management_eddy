4d5f4c9fe60a3ec0773ed6f37269c575a844981b
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-14 15:41:45 +0200

    finish with the user authentication for login and sign up

--- system_management/account/views.py
+++ system_management/account/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .models import User
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email', '')
+        password = request.POST.get('password', '')
+
+        if email and password:
+           user = authenticate(email=email, password=password)
+
+           print('User', user)
+        
+           return redirect('/')
+
+
+    return render(request, 'account/login.html')
+
+def signup(request):
+    if request.method == 'POST':
+      name = request.POST.get('name', '')
+      email = request.POST.get('email', '')
+      password1 = request.POST.get('password1', '')
+      password2 = request.POST.get('password2', '')
+
+      if name and email and password1 and password2:
+        user = User.objects.create_user(name, email, password1)
+
+        print('user created')
+
+        return redirect('/login/')
+      else:
+         print('something went wrong')
+    else:
+       print('just show the form')
+
+    return render(request, 'account/signup.html')
