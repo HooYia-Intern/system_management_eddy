f4fea083b45b4bbfd2e0fbfb1c557f16d158ff62
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-16 14:09:48 +0200

    created the details page for now

--- system_management/todolist/urls.py
+++ system_management/todolist/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
 from . import views
 
-
-app_name= 'todolist'
+app_name = 'todolist'
 
 urlpatterns = [
     path('add/', views.add, name='add'),
+    path('<uuid:pk>/', views.todolist, name='todlist'),  # Adjusted pattern
 ]
