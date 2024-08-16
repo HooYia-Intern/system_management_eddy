36b5bee28961d579384d77ead9e7b290a05b4056
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2024-08-16 18:24:04 +0200

    task can be edited

--- system_management/task/views.py
+++ system_management/task/views.py
@@ -40,3 +40,30 @@ def detail(request, project_id, todolist_id, pk):
     return render(request, 'task/detail.html',{
         'task':task,
     })
+
+
+@login_required
+def edit(request, project_id, todolist_id, pk):
+    # Fetching the related project and todolist
+    project = Project.objects.filter(created_by=request.user).get(pk=project_id)
+    todolist = Todolist.objects.filter(project=project).get(pk=todolist_id)
+
+    # Fetching the specific task to edit
+    task = Task.objects.filter(project=project).filter(todolist=todolist).get(pk=pk)
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+
+        if name:
+            task.name = name
+            task.description = description
+            task.save()
+
+            # Redirect after successfully saving the changes
+            return redirect(f'/projects/{project_id}/{todolist_id}/{pk}')
+
+    # Rendering the edit form if the request method is GET or POST fails validation
+    return render(request, 'task/edit.html', {
+        'task': task
+    })
